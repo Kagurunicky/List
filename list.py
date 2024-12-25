4a684dc6e9669f270a016a0866a0feb1ef00e043
nicole <kagurunicky@gmail.com>
2024-12-26 00:26:46 +0300

    week 2 pyhthon

---
+++ list.py
@@ -0,0 +1,29 @@
+
+#  Creating an empty list
+list = []
+
+# Appending elements to the list
+list.append(10)
+list.append(20)
+list.append(30)
+list.append(40)
+
+# Inserting value 15 at the second position (index 1)
+list.insert(1, 15)
+
+# Extending the list with [50, 60, 70]
+list.extend([50, 60, 70])
+
+# Removing the last element from the list
+list.pop()
+
+# Sorting the list in ascending order
+list.sort()
+
+# Finding the index of the value 30
+index_of_30 = list.index(30)
+
+# Results
+print("Final List:", list)
+print("Index of 30:", index_of_30)
+C:\Users\HP ELITEBOOK 840 G6\Desktop\PLP PYTHON\week 1 python
